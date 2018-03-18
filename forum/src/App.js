a35283f21fdfb28f6d349dbe47ca89d11140db18
kapistelijaKrisu <krisuppi@yahoo.com>
2018-03-18 23:00:19 +0200

    init finished

---
+++ forum/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          hellow world
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App;
