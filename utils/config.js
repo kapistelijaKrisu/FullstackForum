36d1b22b9cd14db2e1786cb5b3a766950a987ed9
kapistelijaKrisu <krisuppi@yahoo.com>
2018-05-15 10:51:10 +0300

    heroku debuggin

--- utils/config.js
+++ utils/config.js
@@ -1,17 +1,23 @@
 
+let port = null
+let dbport = null
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
+  if (process.env.NODE_ENV === 'test') {
+    port = process.env.TEST_PORT
+    dbPort = process.env.DEV_DB
+  } else {
+    port = process.env.DEV_PORT
+    dbPort = process.env.DEV_DB
+  }
+} else {
+  port = process.env.PORT
+  dbPort = process.env.DATABASE_URL
 }
 
-let port = null
 //sql tähä
-if (process.env.NODE_ENV === 'test') {
-  port = process.env.TEST_PORT
-  dbPort = process.env.DEV_DB
-} else {
-  port = process.env.DEV_PORT
-  dbPort = process.env.DEV_DB
-}
+
 
 
 module.exports = {
